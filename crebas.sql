6db893fc90eb8129ce1efb974f5666305c834abc
Aldi Ramadhan <54713281+baboon26@users.noreply.github.com>
2019-08-30 22:00:11 +0700

    Add files via upload

---
+++ crebas.sql
@@ -0,0 +1,67 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     30/08/2019 18:54:49                          */
+/*==============================================================*/
+
+
+drop table if exists TBL_BERANGGOTAKAN;
+
+drop table if exists TBL_DIVISION;
+
+drop table if exists TBL_EMPLOYEE;
+
+drop table if exists TBL_PRESENSI;
+
+/*==============================================================*/
+/* Table: TBL_BERANGGOTAKAN                                     */
+/*==============================================================*/
+create table TBL_BERANGGOTAKAN
+(
+   DIVISION_ID          varchar(20) not null,
+   EMPLOYEE_ID          varchar(10) not null,
+   primary key (DIVISION_ID, EMPLOYEE_ID)
+);
+
+/*==============================================================*/
+/* Table: TBL_DIVISION                                          */
+/*==============================================================*/
+create table TBL_DIVISION
+(
+   DIVISION_ID          varchar(20) not null,
+   DIVISION_NAME        varchar(20),
+   primary key (DIVISION_ID)
+);
+
+/*==============================================================*/
+/* Table: TBL_EMPLOYEE                                          */
+/*==============================================================*/
+create table TBL_EMPLOYEE
+(
+   EMPLOYEE_ID          varchar(10) not null,
+   ARRIVAL_TIME         datetime,
+   EMPLOYEE_NAME        varchar(20),
+   primary key (EMPLOYEE_ID)
+);
+
+/*==============================================================*/
+/* Table: TBL_PRESENSI                                          */
+/*==============================================================*/
+create table TBL_PRESENSI
+(
+   ARRIVAL_TIME         datetime not null,
+   EMPLOYEE_ID          varchar(10),
+   primary key (ARRIVAL_TIME)
+);
+
+alter table TBL_BERANGGOTAKAN add constraint FK_BERANGGOTAKAN foreign key (DIVISION_ID)
+      references TBL_DIVISION (DIVISION_ID) on delete cascade on update restrict;
+
+alter table TBL_BERANGGOTAKAN add constraint FK_BERANGGOTAKAN2 foreign key (EMPLOYEE_ID)
+      references TBL_EMPLOYEE (EMPLOYEE_ID) on delete cascade on update restrict;
+
+alter table TBL_EMPLOYEE add constraint FK_MELAKUKAN foreign key (ARRIVAL_TIME)
+      references TBL_PRESENSI (ARRIVAL_TIME) on delete cascade on update restrict;
+
+alter table TBL_PRESENSI add constraint FK_MELAKUKAN2 foreign key (EMPLOYEE_ID)
+      references TBL_EMPLOYEE (EMPLOYEE_ID) on delete cascade on update restrict;
+
